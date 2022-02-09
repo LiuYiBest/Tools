1bd680c54c8d801a14ae8f1f652ac7f9d9d5adea
liuyidiao <1070692044@qq.com>
2022-02-09 22:18:14 +0800

    commander

---
+++ index.js
@@ -0,0 +1,9 @@
+const options = program.opts();
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --small', 'small pizza size')
+    .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
