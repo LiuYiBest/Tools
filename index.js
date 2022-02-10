79d0c0c8d597c9c1298806345f9eb5a7929285a1
liuyidiao <1070692044@qq.com>
2022-02-10 21:56:24 +0800

    自定义commander

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const options = program.opts();
 
 program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --small', 'small pizza size')
-    .option('-p, --pizza-type <type>', 'flavour of pizza');
+    .option('-x, --xxx', 'the xxx')
+//子命令
+program
+    .command('clone <source> [destination]')
+    .description('clone a repository into a newly created directory')
+    .action((source, destination) => {
+        console.log('clone command called');
+    });
 
 program.parse(process.argv);
 
