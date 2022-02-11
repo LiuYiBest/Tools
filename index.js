a44783bd0389156cd79d4910c566ee7e87afa759
liuyidiao <1070692044@qq.com>
2022-02-11 21:35:06 +0800

    创建命令行

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const program = require('commander');
-
-program
-    .option('-x, --xxx', 'the xxx')
-// 子命令
-//添加任务
-program
-    .command('add')
-    .description('添加一个任务')
-    .action((...args) => {
-        let  words = args.slice(0,-1).join(' ')
-        console.log(words);
-    });
-//删除任务
-program
-    .command('clear')
-    .description('clear all tasks')
-    .action((...args) => {
-        console.log('tasks');
-    });
-
-program.parse(process.argv);
-console.log(program.xxx)
-
+module.exports.add=()=>{
+    console.log('111')
+}
