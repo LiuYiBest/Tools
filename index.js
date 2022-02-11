ddb7d7b0815f148fdf848723802daeee64cc11f9
liuyidiao <1070692044@qq.com>
2022-02-11 22:09:29 +0800

    写入数据

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ module.exports.add=(title)=>{
     // 读取之前的任务
     fs.readFile(dbPath,{flag: 'a+'},(error, data)=>{
         console.log('11')
-        console.log(data.toString())
+        if(error){console.log(error)}else{
+            let list
+            try{
+                let list = JSON.parse(data.toString())
+            }catch (error2){
+                list = []
+            }
+            console.log(list)
+            const task = {
+                title:title,
+                done: false
+            }
+            list.push(task)
+            const  string =JSON.parse(list)
+            fs.writeFile(dbPath,string,(error3)=>{
+                if(error){console.log(error3)}
+            })
+        }
     })
     //往里面添加一个title任务
 }
