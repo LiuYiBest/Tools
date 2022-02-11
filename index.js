fccb972ea8984c83a3cd6617d28fa0e43406362f
liuyidiao <1070692044@qq.com>
2022-02-11 22:05:59 +0800

    本地创建todo

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const home = process.env.HOME
+const homedir = require('os').homedir();
+const home = process.env.HOME || homedir;
 const p = require('path')
 const fs = require('fs')
 const dbPath = p.join(home,'.todo')
 
 module.exports.add=(title)=>{
     // 读取之前的任务
-    fs.readFile(dbPath,{flat: 'a+'},(error,data)=>{
+    fs.readFile(dbPath,{flag: 'a+'},(error, data)=>{
+        console.log('11')
         console.log(data.toString())
     })
     //往里面添加一个title任务
