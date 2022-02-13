3de08fd6b3accb45ae915ad1a486ee3fc2d4ed26
liuyidiao <1070692044@qq.com>
2022-02-13 21:06:53 +0800

    测试用例

--- __test__/db.spec.js
+++ __test__/db.spec.js
@@ -1,4 +1,8 @@
+const db = require('../db')
+
 describe('db',()=>{
-    it('read',()=>{})
+    it('read',()=>{
+        expect(db.read instanceof Function).toBe(true)  //是否是函数的实例
+    })
     it('read',()=>{})
 })
